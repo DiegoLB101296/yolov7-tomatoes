5b5a0b8df65eeb9697c2221142c98350da7082d7
DiegoLB96 <112820760+DiegoLB101296@users.noreply.github.com>
2023-06-13 14:29:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import json
 from PIL import Image
 import cv2
+import pickle
 
 import torch
 from flask import Flask, jsonify, url_for, render_template, request, redirect, Response
@@ -14,17 +15,17 @@ RESULT_FOLDER = os.path.join('static')
 app.config['RESULT_FOLDER'] = RESULT_FOLDER
 
 # finds the model inside your directory automatically - works only if there is one model
-def find_model():
-    for f  in os.listdir():
-        if f.endswith(".pt"):
-            return f
-    print("please place a model file in this directory!")
+#def find_model():
+#    for f  in os.listdir():
+#        if f.endswith(".pt"):
+#            return f
+#    print("please place a model file in this directory!")
     
-model_name = find_model()
-model =torch.hub.load("WongKinYiu/yolov7", 'custom',model_name)
-
+#model_name = find_model()
+#model =torch.hub.load("WongKinYiu/yolov7", 'custom',model_name)
+model = pickle.load(open('model.pkl', 'rb'))
 model.eval()
-
+#pickle.dump(model, open('model.pkl', 'wb'))
 def get_prediction(img_bytes):
     img = Image.open(io.BytesIO(img_bytes))
     imgs = [img]  # batched list of images
@@ -46,7 +47,7 @@ def predict():
         results.save(save_dir='static')
         filename = 'image0.jpg'
         
-        return render_template('result.html',result_image = filename,model_name = model_name)
+        return render_template('result.html',result_image = filename,model_name = model)
         #return render_template('result.html',result_image = results,model_name = model_name)
 
     return render_template('index.html')
